55806b2c95278537f574ac299f2520dd540907af
Adilson <usadilsondigital@gmail.com>
2024-03-13 23:26:04 -0500

    calculate the interval

--- ClassTimeSpan.cs
+++ ClassTimeSpan.cs
@@ -13,6 +13,12 @@ namespace Donet8
             // Define two dates.
             DateTime date1 = new DateTime(2010, 1, 1, 8, 0, 15);
             DateTime date2 = new DateTime(2010, 8, 18, 13, 30, 30);
+
+            // Calculate the interval between the two dates.
+            TimeSpan interval = date2 - date1;
+            Console.WriteLine("{0} - {1} = {2}", date2, date1, interval.ToString());
+
+
         }
     }
 }
