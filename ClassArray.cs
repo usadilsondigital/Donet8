d95bf45ea6eeda2406d17561ddf1dcf152a34f45
Adilson <usadilsondigital@gmail.com>
2024-03-13 23:31:15 -0500

    class array

---
+++ ClassArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donet8
+{
+    //Provides methods for creating,
+    //manipulating, searching, and sorting arrays, thereby serving as the base class for all arrays in the common language runtime.
+    public class ClassArray
+    {
+    }
+}
