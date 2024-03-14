7c3f2e3c4063bac21d49bf1538f2f9282bbf6365
Adilson <usadilsondigital@gmail.com>
2024-03-13 23:25:35 -0500

    define 2 dates

--- ClassTimeSpan.cs
+++ ClassTimeSpan.cs
@@ -9,5 +9,10 @@ namespace Donet8
     //Struct that Represents a time interval.
     public class ClassTimeSpan
     {
+        public void TestCase() {
+            // Define two dates.
+            DateTime date1 = new DateTime(2010, 1, 1, 8, 0, 15);
+            DateTime date2 = new DateTime(2010, 8, 18, 13, 30, 30);
+        }
     }
 }
