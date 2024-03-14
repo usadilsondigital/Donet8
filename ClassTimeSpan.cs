b88bcbc2bbc647cb9c2b25a4d7d7da7ec8323b59
Adilson <usadilsondigital@gmail.com>
2024-03-13 23:24:56 -0500

    time span docs

---
+++ ClassTimeSpan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donet8
+{
+    //Struct that Represents a time interval.
+    public class ClassTimeSpan
+    {
+    }
+}
