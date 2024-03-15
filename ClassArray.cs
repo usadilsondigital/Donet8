3a79a56587a421656542602fe8733be18e0609ff
Adilson <usadilsondigital@gmail.com>
2024-03-14 23:35:14 -0500

    long length docs

--- ClassArray.cs
+++ ClassArray.cs
@@ -165,6 +165,6 @@ namespace Donet8
 
         public int Length { get; }//Gets the total number of elements in all the dimensions of the Array.
 
-        public long LongLength { get; }
+        public long LongLength { get; }//Gets a 64-bit integer that represents the total number of elements in all the dimensions of the Array.
     }
 }
