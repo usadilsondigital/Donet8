6771b81903f61d279410b41f3db244f9373d3661
Adilson <usadilsondigital@gmail.com>
2024-03-16 23:44:27 -0500

    docs

--- ClassArray.cs
+++ ClassArray.cs
@@ -220,6 +220,7 @@ namespace Donet8
         //Performs the specified action on each element of the specified array.
         public static void ForEach<T>(T[] array, Action<T> action);
 
+        //Returns an IEnumerator for the Array.
         public System.Collections.IEnumerator GetEnumerator();
 
 
