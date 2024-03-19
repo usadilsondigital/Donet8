e06897e40dd837e4a5c3b7307e919680425ed318
Adilson <usadilsondigital@gmail.com>
2024-03-18 22:11:19 -0500

    time zone time struct

---
+++ TimeZoneTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donet8
+{
+    public class TimeZoneTime
+    {
+        private DateTimeOffset dt;
+        private TimeZoneInfo tz;
+
+        public TimeZoneTime(DateTimeOffset dateTime, TimeZoneInfo timeZone)
+        {
+            dt = dateTime;
+            tz = timeZone;
+        }
+
+        public DateTimeOffset DateTime
+        { get { return dt; } }
+
+        public TimeZoneInfo TimeZone
+        { get { return tz; } }
+    }
+}
