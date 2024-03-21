e5096985126f75b524b543aeea38cfadd66c8da3
Adilson <usadilsondigital@gmail.com>
2024-03-20 22:41:55 -0500

    example convert all

---
+++ ExampleConvertAll.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donet8
+{
+    public class ExampleConvertAll
+    {
+        public static void Main()
+        {
+            // Create an array of PointF objects.
+            PointF[] apf = {
+            new PointF(27.8F, 32.62F),
+            new PointF(99.3F, 147.273F),
+            new PointF(7.5F, 1412.2F) };
+        }
+}
