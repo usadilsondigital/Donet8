9b8bd553ac48bcf51939b061fccbeb80c4614981
Adilson <usadilsondigital@gmail.com>
2024-03-20 22:53:45 -0500

    example exists

---
+++ ExampleExists.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donet8
+{
+    public class ExampleExists
+    {
+        public void example(string[] args)
+        {
+            string[] planets = { "Mercury", "Venus",
+                "Earth", "Mars", "Jupiter",
+                "Saturn", "Uranus", "Neptune" };
+
+        }
+    }
+}
