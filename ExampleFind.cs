3608185fc326825bc048b3dc2a8404e4a03bab18
Adilson <usadilsondigital@gmail.com>
2024-03-21 23:36:44 -0500

    example find class

---
+++ ExampleFind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donet8
+{
+    public class ExampleFind
+    {
+        public  void example1()
+        {
+            // Create an array of five Point structures.
+            Point[] points = { new Point(100, 200),
+            new Point(150, 250), new Point(250, 375),
+            new Point(275, 395), new Point(295, 450) };
+        }
+
+    }
+}
