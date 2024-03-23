57c1e290564c1cad0ecd240437200cc1614944eb
Adilson <usadilsondigital@gmail.com>
2024-03-22 23:47:47 -0500

    console find last

--- DinoDiscoverySet.cs
+++ DinoDiscoverySet.cs
@@ -54,6 +54,7 @@ namespace Donet8
             Console.WriteLine( "\nArray.Exists(dinosaurs, \"{0}\"): {1}",Ending,Array.Exists(dinosaurs, dinoType));
             Console.WriteLine("\nArray.TrueForAll(dinosaurs, \"{0}\"): {1}",Ending,Array.TrueForAll(dinosaurs, dinoType));
             Console.WriteLine("\nArray.Find(dinosaurs, \"{0}\"): {1}",Ending,Array.Find(dinosaurs, dinoType));
+            Console.WriteLine("\nArray.FindLast(dinosaurs, \"{0}\"): {1}",Ending,Array.FindLast(dinosaurs, dinoType));
 
         }
 
