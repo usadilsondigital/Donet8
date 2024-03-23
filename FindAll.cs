74b2d49487dc0cf439130d2dae3eb12003c0f014
Adilson <usadilsondigital@gmail.com>
2024-03-22 23:25:23 -0500

    example

---
+++ FindAll.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donet8
+{
+    public class FindAll
+    {
+
+        public void example1()
+        {
+            // Get an array of n random integers.
+            int[] values = GetArray(50, 0, 1000);
+            int lBound = 300;
+            int uBound = 600;
+        }
+
+    }
+}
