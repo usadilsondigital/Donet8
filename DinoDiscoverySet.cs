c6927da957fdd28e917b496f55fca948ecf4c0d1
Adilson <usadilsondigital@gmail.com>
2024-03-22 23:38:57 -0500

    dino class

---
+++ DinoDiscoverySet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donet8
+{
+    public class DinoDiscoverySet
+    {
+        public static void Main()
+        {
+            string[] dinosaurs =
+            {
+            "Compsognathus", "Amargasaurus", "Oviraptor",
+            "Velociraptor",  "Deinonychus",  "Dilophosaurus",
+            "Gallimimus",    "Triceratops"
+        };
+
+        }
+    }
+}
