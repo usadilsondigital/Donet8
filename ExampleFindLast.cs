b44ab316251e217a87aae23f3ad396d00e44b4e7
Adilson <usadilsondigital@gmail.com>
2024-03-23 22:47:50 -0500

    console array

--- ExampleFindLast.cs
+++ ExampleFindLast.cs
@@ -58,11 +58,15 @@ namespace Donet8
 
             Console.WriteLine("\nArray.Exists(dinosaurs, \"{0}\"): {1}",Ending,Array.Exists(dinosaurs, dinoType));
 
+            Console.WriteLine(
+            "\nArray.TrueForAll(dinosaurs, \"{0}\"): {1}",
+            Ending,
+            Array.TrueForAll(dinosaurs, dinoType));
 
 
 
 
         }
 
-        }
+    }
     }
