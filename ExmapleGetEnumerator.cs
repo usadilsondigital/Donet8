fe8317d4242d5e1db08bed76f75f13c427651d4c
Adilson <usadilsondigital@gmail.com>
2024-03-24 23:29:57 -0500

    myArr

--- ExmapleGetEnumerator.cs
+++ ExmapleGetEnumerator.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Donet8
 {
-    internal class ExmapleGetEnumerator
+    public class ExmapleGetEnumerator
     {
+        public  void example()
+        {
+
+            // Creates and initializes a new Array.
+            String[] myArr = new String[10];
+            myArr[0] = "The";
+            myArr[1] = "quick";
+            myArr[2] = "brown";
+            myArr[3] = "fox";
+            myArr[4] = "jumps";
+            myArr[5] = "over";
+            myArr[6] = "the";
+            myArr[7] = "lazy";
+            myArr[8] = "dog";
+        }
+
+
+
+
     }
 }
