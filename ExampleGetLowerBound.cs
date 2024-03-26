bc0cd2a72af983b1b42d61e6ab41c1fd57fee9fe
Adilson <usadilsondigital@gmail.com>
2024-03-25 23:31:42 -0500

    example method, 1 dim array

--- ExampleGetLowerBound.cs
+++ ExampleGetLowerBound.cs
@@ -8,5 +8,16 @@ namespace Donet8
 {
     public class ExampleGetLowerBound
     {
+        public  void Example()
+        {
+            // Create a one-dimensional integer array.
+            int[] integers = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+     
+        
+        
+        }
+    
+    
     }
+
 }
