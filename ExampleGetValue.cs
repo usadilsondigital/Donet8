71bcf1aae21e990e7747c9694f06575152a8daa4
Adilson <usadilsondigital@gmail.com>
2024-03-27 22:18:52 -0500

    output

--- ExampleGetValue.cs
+++ ExampleGetValue.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Donet8
 {
-    internal class ExampleGetValue
+    public class ExampleGetValue
     {
+
+
+
+        public  void example()
+        {
+
+            
+        }
     }
+
+
+    /*
+    This code produces the following output.
+
+    [3]:   three
+    [1,3]:   one-three
+    [1,2,3]:   one-two-three
+    [1,2,3,0,1,2,3]:   one-two-three-zero-one-two-three
+
+    */
+
+
+
+
 }
