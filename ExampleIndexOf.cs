60edcad3bea323181fd769a3dd19cbcdb238d925
Adilson <usadilsondigital@gmail.com>
2024-03-27 22:26:27 -0500

    create an array

--- ExampleIndexOf.cs
+++ ExampleIndexOf.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Donet8
 {
-    internal class ExampleIndexOf
+    public class ExampleIndexOf
     {
+        // Create a string array with 3 elements having the same value.
+        String[] strings = { "the", "quick", "brown", "fox", "jumps",
+                     "over", "the", "lazy", "dog", "in", "the",
+                     "barn" };
     }
 }
