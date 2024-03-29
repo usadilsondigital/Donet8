97def0ba104eacf06bf74affdebb6666af19ec1b
Adilson <usadilsondigital@gmail.com>
2024-03-28 23:05:35 -0500

    get lower bouwn loop

--- ExampleIndexOf.cs
+++ ExampleIndexOf.cs
@@ -9,8 +9,13 @@ namespace Donet8
     public class ExampleIndexOf
     {
         // Create a string array with 3 elements having the same value.
-        String[] strings = { "the", "quick", "brown", "fox", "jumps",
-                     "over", "the", "lazy", "dog", "in", "the",
-                     "barn" };
+        String[] strings = { "the", "quick", "brown", "fox", "jumps","over", "the", "lazy", "dog", "in", "the","barn" };
+
+        // Display the elements of the array.
+        Console.WriteLine("The array contains the following values:");
+        for (int i = strings.GetLowerBound(0); i <= strings.GetUpperBound(0); i++)
+           Console.WriteLine("   [{0,2}]: {1}", i, strings[i]);
+
+
     }
 }
