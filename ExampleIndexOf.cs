9ad4bec407daaf2ecd5f8db8f88dbaa5c4a9d012
Adilson <usadilsondigital@gmail.com>
2024-03-28 23:10:55 -0500

    output

--- ExampleIndexOf.cs
+++ ExampleIndexOf.cs
@@ -36,6 +36,24 @@ namespace Donet8
             Console.WriteLine("The first occurrence of \"{0}\" between index {1} and index {2} is at index {3}.",
                   searchString, position, strings.GetUpperBound(0), index);
 
+            // The example displays the following output:
+                //    The array contains the following values:
+                //       [ 0]: the
+                //       [ 1]: quick
+                //       [ 2]: brown
+                //       [ 3]: fox
+                //       [ 4]: jumps
+                //       [ 5]: over
+                //       [ 6]: the
+                //       [ 7]: lazy
+                //       [ 8]: dog
+                //       [ 9]: in
+                //       [10]: the
+                //       [11]: barn
+                //    The first occurrence of "the" is at index 0.
+                //    The first occurrence of "the" between index 4 and the end is at index 6.
+                //    The first occurrence of "the" between index 7 and index 11 is at index 10.
+
 
     }
 }
