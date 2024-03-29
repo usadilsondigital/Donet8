896a7b77917ffcc58c3924573c8594e0cf63f45b
Adilson <usadilsondigital@gmail.com>
2024-03-28 23:18:15 -0500

    create a new array

--- ExampleLastIndexOf.cs
+++ ExampleLastIndexOf.cs
@@ -8,5 +8,12 @@ namespace Donet8
 {
     public class ExampleLastIndexOf
     {
+        // Creates and initializes a new Array with three elements of the same value.
+        Array myArray = Array.CreateInstance(typeof(string), 12);
+        myArray.SetValue( "the", 0 );
+        myArray.SetValue( "quick", 1 );
+        myArray.SetValue( "brown", 2 );
+        myArray.SetValue( "fox", 3 );
+        myArray.SetValue( "jumps", 4 );
     }
 }
