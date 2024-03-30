bbbaa3cd4d15b9501811460ad3e93a9dc6bc2bdc
Adilson <usadilsondigital@gmail.com>
2024-03-29 23:46:43 -0500

    output1

--- ExampleResize.cs
+++ ExampleResize.cs
@@ -56,7 +56,20 @@ namespace Donet8
             Console.WriteLine();
         }
 
+        /*
+This code produces the following output.
 
+The string array initially contains the following values:
+   [0] : The
+   [1] : quick
+   [2] : brown
+   [3] : fox
+   [4] : jumps
+   [5] : over
+   [6] : the
+   [7] : lazy
+   [8] : dog
+        */
 
 
 
