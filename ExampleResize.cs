e5dbfce7fda9384e24f6502193d2b03df3e20e2e
Adilson <usadilsondigital@gmail.com>
2024-03-29 23:47:52 -0500

    output to smaller

--- ExampleResize.cs
+++ ExampleResize.cs
@@ -71,24 +71,33 @@ The string array initially contains the following values:
    [8] : dog
         */
 
-/*After resizing to a larger size,
+        /*After resizing to a larger size,
+        the string array contains the following values:
+        [0] : The
+        [1] : quick
+        [2] : brown
+        [3] : fox
+        [4] : jumps
+        [5] : over
+        [6] : the
+        [7] : lazy
+        [8] : dog
+        [9] :
+        [10] :
+        [11] :
+        [12] :
+        [13] :*/
+
+        /*After resizing to a smaller size,
 the string array contains the following values:
-[0] : The
-[1] : quick
-[2] : brown
-[3] : fox
-[4] : jumps
-[5] : over
-[6] : the
-[7] : lazy
-[8] : dog
-[9] :
-[10] :
-[11] :
-[12] :
-[13] :*/
+   [0] : The
+   [1] : quick
+   [2] : brown
+   [3] : fox
+
+*/
 
 
 
-}
+    }
 }
