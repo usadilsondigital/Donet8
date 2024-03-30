410e83584d48f580274a2cbd32b9804571c30c38
Adilson <usadilsondigital@gmail.com>
2024-03-29 23:49:05 -0500

    create myarray

--- ExampleReverse .cs
+++ ExampleReverse .cs
@@ -8,5 +8,19 @@ namespace Donet8
 {
     public class ExampleReverse
     {
+
+        public void example() {
+            // Creates and initializes a new Array.
+            Array myArray = Array.CreateInstance(typeof(string), 9);
+            myArray.SetValue("The", 0);
+            myArray.SetValue("quick", 1);
+            myArray.SetValue("brown", 2);
+            myArray.SetValue("fox", 3);
+            myArray.SetValue("jumps", 4);
+            myArray.SetValue("over", 5);
+            myArray.SetValue("the", 6);
+            myArray.SetValue("lazy", 7);
+            myArray.SetValue("dog", 8);
+        }
     }
 }
