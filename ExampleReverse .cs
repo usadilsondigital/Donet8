c62d9d032db97bc5632213a4ae9e1352927804ae
Adilson <usadilsondigital@gmail.com>
2024-03-31 10:48:23 -0500

    print method

--- ExampleReverse .cs
+++ ExampleReverse .cs
@@ -168,5 +168,11 @@ namespace Donet8
             PrintIndexAndValues(myArray);
         }
 
+        public static void PrintIndexAndValues(Array myArray)
+        {
+            for (int i = myArray.GetLowerBound(0); i <= myArray.GetUpperBound(0); i++)
+                Console.WriteLine("\t[{0}]:\t{1}", i, myArray.GetValue(i));
         }
+
+    }
 }
