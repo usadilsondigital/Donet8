ce662cf37555246ca81377f931d4217b4b67557b
Adilson <usadilsondigital@gmail.com>
2024-04-08 22:29:40 -0500

    public

--- ArrayIterfaces.cs
+++ ArrayIterfaces.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Donet8
 {
-    internal class ArrayIterfaces
+    public class ArrayIterfaces
     {
         //Array.ICollection.Count//Gets the number of elements contained in the Array.
         //Array.IList.Add//implementation adds a member to a collection
