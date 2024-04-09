a44d3c1dd59d058faa9e9191a8abcf37fa540216
Adilson <usadilsondigital@gmail.com>
2024-04-08 22:18:38 -0500

    index of

--- ArrayIterfaces.cs
+++ ArrayIterfaces.cs
@@ -12,5 +12,6 @@ namespace Donet8
         //Array.IList.Add//implementation adds a member to a collection
         //Array.IList.Clear//Removes all items from the IList.
         //Array.IList.Contains(Object)//Determines whether an element is in the IList.
+        //Array.IList.IndexOf(Object)
     }
 }
