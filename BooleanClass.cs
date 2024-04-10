a311263e1c99ff5176486628981b250c237ef8cc
Adilson <usadilsondigital@gmail.com>
2024-04-09 23:05:59 -0500

    boolean compare to docs

--- BooleanClass.cs
+++ BooleanClass.cs
@@ -9,5 +9,7 @@ namespace Donet8
     public class BooleanClass
     {
         //Boolean.CompareTo
+        //Compares this instance to a specified
+        //object or Boolean object and returns an integer that indicates their relationship to one another.
     }
 }
