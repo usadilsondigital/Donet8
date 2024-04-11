2b2746dc4e3e10d9b87e7c8420388f26747ee7d7
Adilson <usadilsondigital@gmail.com>
2024-04-11 18:26:02 -0500

    out boolean

--- BooleanClass.cs
+++ BooleanClass.cs
@@ -111,6 +111,7 @@ namespace Donet8
         This example produces the following results:
                 The following is the result of using the generic and non-generic versions of the
         CompareTo method for several base types:
+        Boolean:  True is equal to True
         */
 
 
