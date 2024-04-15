b18080915d585ca9edffad9f04d21d22be77b7fb
Adilson <usadilsondigital@gmail.com>
2024-04-14 23:22:09 -0500

    raining output

--- BooleanClass.cs
+++ BooleanClass.cs
@@ -163,6 +163,7 @@ namespace Donet8
             Console.WriteLine("raining.ToString() returns {0}", raining);
             Console.WriteLine("busLate.ToString() returns {0}", busLate);
             // The example displays the following output:
+            //       raining.ToString() returns False
 
         }
 
