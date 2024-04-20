b7224342c3c955b9d52b93f19cb06bbc0212a2d3
Adilson <usadilsondigital@gmail.com>
2024-04-19 22:40:05 -0500

    long [] dest

--- BufferClass.cs
+++ BufferClass.cs
@@ -102,6 +102,9 @@ namespace Donet8
         {
             // These are the source and destination arrays for BlockCopy.
             short[] src = { 258, 259, 260, 261, 262, 263, 264,265, 266, 267, 268, 269, 270 };
+            long[] dest = { 17, 18, 19, 20 };
+
+
         }
 
 
