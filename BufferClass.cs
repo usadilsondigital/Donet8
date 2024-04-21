f0c5f4910d07b269b7dd4235be30ee3567bbb802
Adilson <usadilsondigital@gmail.com>
2024-04-21 17:58:35 -0500

    loop arr

--- BufferClass.cs
+++ BufferClass.cs
@@ -173,6 +173,8 @@ namespace Donet8
             const int INT_SIZE = 4;
             int[] arr = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
             Buffer.BlockCopy(arr, 0 * INT_SIZE, arr, 3 * INT_SIZE, 4 * INT_SIZE);
+            foreach (int value in arr)
+
 
         }
 
