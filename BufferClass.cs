dc2dd50ac894cbb294a1cfdf2e76b1ea03cf611c
Adilson <usadilsondigital@gmail.com>
2024-04-21 17:58:15 -0500

    buffer copy

--- BufferClass.cs
+++ BufferClass.cs
@@ -172,7 +172,7 @@ namespace Donet8
         public void exampleBufferCopy() {
             const int INT_SIZE = 4;
             int[] arr = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-
+            Buffer.BlockCopy(arr, 0 * INT_SIZE, arr, 3 * INT_SIZE, 4 * INT_SIZE);
 
         }
 
