bc04175078940599d1a93eb840a1547adb2a41f7
Adilson <usadilsondigital@gmail.com>
2024-04-25 22:45:15 -0500

    system. double[]

--- BufferClass.cs
+++ BufferClass.cs
@@ -218,7 +218,7 @@ namespace Donet8
             chars           System.Char[]       5           10
             shorts          System.Int16[]      6           12
             singles         System.Single[]     5           20
-            doubles
+            doubles         System.Double[] 
 
 
 
