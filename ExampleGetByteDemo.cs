91640fed56bd0aa2d9f0371ec3b397e975dfb9d7
Adilson <usadilsondigital@gmail.com>
2024-04-29 00:28:42 -0500

    output note

--- ExampleGetByteDemo.cs
+++ ExampleGetByteDemo.cs
@@ -71,8 +71,12 @@ namespace Donet8
             ArrayInfo(longs, "longs", 21);
 
             /*
-This example of the Buffer.GetByte( Array, int )
+            This example of the Buffer.GetByte( Array, int )
             method generates the following output.
+            Note: The arrays are displayed from right to left.
+
+
+
 
              */
 
