f954a46173a29dea4a3e2d198593d4be8bbbc197
Adilson <usadilsondigital@gmail.com>
2024-04-28 23:01:22 -0500

    display method

--- ExampleGetByteDemo.cs
+++ ExampleGetByteDemo.cs
@@ -10,5 +10,9 @@ namespace Donet8
     {
         const string formatter = "{0,10}{1,10}{2,9} {3}";
         // Display the array contents in hexadecimal
+        public static void DisplayArray(Array arr, string name)
+        {
+         
+        }
     }
 }
