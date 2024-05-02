bef9335dcc94071acc95ff8237567e8681fde5ea
Adilson <usadilsondigital@gmail.com>
2024-05-01 22:46:35 -0500

    0x0D

--- ExampleGetByteDemo.cs
+++ ExampleGetByteDemo.cs
@@ -83,7 +83,7 @@ namespace Donet8
             Array       index   value
             -----     -----    ----- ----
             ints         0      199   0xC7
-            ints         7       13
+            ints         7       13   0x0D
 
 
              */
