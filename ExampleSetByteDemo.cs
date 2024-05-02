3036005352f1da8e5b09eaf2b3e1a40abdd97b14
Adilson <usadilsondigital@gmail.com>
2024-05-02 08:46:16 -0500

    loop

--- ExampleSetByteDemo.cs
+++ ExampleSetByteDemo.cs
@@ -19,6 +19,8 @@ namespace Donet8
 
             // Display the array elements from right to left.
             Console.Write("{0,7}:", name);
+            for (int loopX = arr.Length - 1; loopX >= 0; loopX--)
+
 
         }
 
