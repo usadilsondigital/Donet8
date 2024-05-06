1f67f4e8a713bfa8937803ed3d1dbb6992f7015d
Adilson <usadilsondigital@gmail.com>
2024-05-05 22:38:07 -0500

    docs max value

--- ByteClass.cs
+++ ByteClass.cs
@@ -8,7 +8,7 @@ namespace Donet8
 {
     public class ByteClass
     {
-
+        //Represents the largest possible value of a Byte. This field is constant.
         public const byte MaxValue = 255;
     }
 }
