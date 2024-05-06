4316ec3b38b821ee5aa2344ae8b3886ba75a664b
Adilson <usadilsondigital@gmail.com>
2024-05-06 08:25:28 -0500

    msg 2

--- ByteClass.cs
+++ ByteClass.cs
@@ -57,6 +57,7 @@ namespace Donet8
 
             string nl = Environment.NewLine;
             string msg = "{0}The following is the result of using the generic and non-generic{0}" +
+                  "versions of the CompareTo method for several base types:{0}";
 
 
 
