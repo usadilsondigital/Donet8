e5772515c565205d4ff4f5251229fc91c8b80927
Adilson <usadilsondigital@gmail.com>
2024-05-06 08:28:57 -0500

    guid

--- ByteClass.cs
+++ ByteClass.cs
@@ -66,6 +66,13 @@ namespace Donet8
             Version versX = new Version("1.2.333.4");
 
             // Guid = CA761232-ED42-11CE-BACD-00AA0057B223
+            Guid guidX = new Guid("{CA761232-ED42-11CE-BACD-00AA0057B223}");
+
+
+
+
+
+
 
         }
 
