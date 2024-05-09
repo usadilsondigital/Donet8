1f5ec5bd5b2cc2b9e5c92a660e4c6e2ede13427d
Adilson <usadilsondigital@gmail.com>
2024-05-08 20:59:57 -0500

    Console.Write(caption);

--- ByteClass.cs
+++ ByteClass.cs
@@ -122,7 +122,8 @@ namespace Donet8
         public static void Show(string caption, Object var1, Object var2,
                            int resultGeneric, int resultNonGeneric)
         { 
-            string relation; 
+            string relation;
+            Console.Write(caption);
         }
 
 
