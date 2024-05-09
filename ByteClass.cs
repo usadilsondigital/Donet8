83d7f2ed49184f05f83ebb09d2580f60716348d5
Adilson <usadilsondigital@gmail.com>
2024-05-08 21:40:38 -0500

    console

--- ByteClass.cs
+++ ByteClass.cs
@@ -136,8 +136,8 @@ namespace Donet8
             // The following condition will never occur because the generic and non-generic
             // CompareTo methods are equivalent.
             else
-            { 
-            
+            {
+                Console.WriteLine("Generic CompareTo = {0}; non-generic CompareTo = {1}",
             }
         }
 
