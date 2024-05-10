8c8bd32df569ad0fd29979db407db63779d88543
Adilson <usadilsondigital@gmail.com>
2024-05-09 23:17:42 -0500

    double

--- ByteClass.cs
+++ ByteClass.cs
@@ -151,7 +151,7 @@ namespace Donet8
         Int64:    4 is greater than -4
         Decimal:  -5.5 is less than 5.5
         Single:   6.6 is equal to 6.6
-
+        Double:   7.7 is greater than -7.7
 
 
 
