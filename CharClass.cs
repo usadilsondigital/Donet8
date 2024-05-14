c78171fcb71eec37c227818265887a88912d40f8
Adilson <usadilsondigital@gmail.com>
2024-05-13 23:33:52 -0500

    zero docs

--- CharClass.cs
+++ CharClass.cs
@@ -80,6 +80,7 @@ namespace Donet8
             Show(s1);
             Console.WriteLine();
             //  Convert the surrogate pair in the string at index position
+            //  zero to a code point.
 
 
 
