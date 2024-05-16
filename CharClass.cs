d45d5980d9b94875d6c5a9b2a092eb6318a59259
Adilson <usadilsondigital@gmail.com>
2024-05-16 11:20:27 -0500

    console.writeline

--- CharClass.cs
+++ CharClass.cs
@@ -208,6 +208,7 @@ namespace Donet8
             int utf32 = 0x10107; // AEGEAN NUMBER ONE
             string surrogate = Char.ConvertFromUtf32(utf32);
             foreach (var ch in surrogate)
+                Console.WriteLine("U+{0:X4}: {1}    ", Convert.ToUInt16(ch),
         }
 
 
