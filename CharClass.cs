6873ebc33765838f86a7aa64063af212048d732f
Adilson <usadilsondigital@gmail.com>
2024-05-19 23:09:50 -0500

    position 90

--- CharClass.cs
+++ CharClass.cs
@@ -248,6 +248,10 @@ namespace Donet8
             //       U+D800 at position 0: 60     U+DD15 at position 1: -1
             //       U+D800 at position 0: 70     U+DD16 at position 1: -1
             //       U+D800 at position 0: 80     U+DD17 at position 1: -1
+            //       U+D800 at position 0: 90     U+DD18 at position 1: -1
+
+
+
 
 
     }
