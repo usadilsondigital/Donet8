3d03c9418f66de0ee2887d2495d6cbf023123173
Adilson <usadilsondigital@gmail.com>
2024-05-23 01:40:13 -0500

    console a2

--- CharClass.cs
+++ CharClass.cs
@@ -352,7 +352,7 @@ namespace Donet8
             Console.WriteLine(divider);
             Console.WriteLine("Is each of the following characters a high surrogate?");
             Console.WriteLine("A1) cLow?  - {0}", Char.IsHighSurrogate(cLow));
-
+            Console.WriteLine("A2) cHigh? - {0}", Char.IsHighSurrogate(cHigh));
 
 
 
