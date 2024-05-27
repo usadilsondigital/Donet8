1cb32772c9540aa061d87038b3a28126920c2753
Adilson <usadilsondigital@gmail.com>
2024-05-27 12:24:28 -0500

    IsLowSurrogate

--- CharClass.cs
+++ CharClass.cs
@@ -436,10 +436,16 @@ This example produces the following results:
             char ch = 'a';
             Console.WriteLine(Char.IsLower(ch));                // Output: "True"
         Console.WriteLine(Char.IsLower("upperCase", 5));    // Output: "False"
+        }
+
+
+    public  void IsLowSurrogate()
+    { 
+    
     }
 
 
 
 
-}
+    }
 }
