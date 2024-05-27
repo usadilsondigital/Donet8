26b871f5a1f62a070380c46168dedb1d6e996028
Adilson <usadilsondigital@gmail.com>
2024-05-26 23:22:26 -0500

    ouput

--- CharClass.cs
+++ CharClass.cs
@@ -368,7 +368,8 @@ namespace Donet8
             Console.WriteLine("C3) s1[1] and s1[2]? - {0}", Char.IsSurrogatePair(s1, 1));
             Console.WriteLine("C4) s1[2] and s1[3]? - {0}", Char.IsSurrogatePair(s1, 2));
             Console.WriteLine(divider);
-
+            /*
+This example produces the following results:*/
 
 
 
