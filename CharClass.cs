d5ee418922e3dc5603664e7a14c998d229c7d25a
Adilson <usadilsondigital@gmail.com>
2024-05-26 23:25:49 -0500

    a2

--- CharClass.cs
+++ CharClass.cs
@@ -383,7 +383,7 @@ This example produces the following results:
             Is each of the following characters a high surrogate?
 
             A1) cLow?  - False
-
+            A2) cHigh? - True
 
 
 
