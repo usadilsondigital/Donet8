062df3a44793e9bb780ac538256ec81ed0243111
Adilson <usadilsondigital@gmail.com>
2024-05-26 23:23:33 -0500

    hexadecimal

--- CharClass.cs
+++ CharClass.cs
@@ -374,6 +374,9 @@ This example produces the following results:
             Hexadecimal code point of the character, cLow:  DC00
 
             Characters in string, s1: 'a', high surrogate, low surrogate, 'z'
+            Hexadecimal code points of the characters in string, s1:
+
+
 
 
              */
