2290e056cc9e49f548fcaf48d78e1951e31e45e4
Adilson <usadilsondigital@gmail.com>
2024-05-26 23:17:08 -0500

    b4

--- CharClass.cs
+++ CharClass.cs
@@ -360,6 +360,7 @@ namespace Donet8
             Console.WriteLine("B1) cLow?  - {0}", Char.IsLowSurrogate(cLow));
             Console.WriteLine("B2) cHigh? - {0}", Char.IsLowSurrogate(cHigh));
             Console.WriteLine("B3) s1[0]? - {0}", Char.IsLowSurrogate(s1, 0));
+            Console.WriteLine("B4) s1[2]? - {0}", Char.IsLowSurrogate(s1, 2));
 
 
 
