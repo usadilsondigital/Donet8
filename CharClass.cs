5c21fa86bd8236d64369ad18cf408e3635268642
Adilson <usadilsondigital@gmail.com>
2024-05-29 12:08:34 -0500

    console a1

--- CharClass.cs
+++ CharClass.cs
@@ -458,6 +458,9 @@ This example produces the following results:
         }
         Console.WriteLine(divider);
         Console.WriteLine("Is each of the following characters a high surrogate?");
+        Console.WriteLine("A1) cLow?  - {0}", Char.IsHighSurrogate(cLow));
+
+
 
 
 
