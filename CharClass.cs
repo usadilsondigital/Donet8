5f20e3fb315ed8bd5fc055b93863cef5f5f0c39c
Adilson <usadilsondigital@gmail.com>
2024-06-02 21:26:35 -0500

    console writeline

--- CharClass.cs
+++ CharClass.cs
@@ -513,6 +513,7 @@ This example produces the following results:
     public  void IsNumberSample()
     {
         string str = "non-numeric";
+        Console.WriteLine(Char.IsNumber('8'));      // Output: "True"
     }
 
 }
