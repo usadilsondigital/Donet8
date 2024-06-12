ec98469753dcf9e619b7364a9901d20a8ab8c5e3
Adilson <usadilsondigital@gmail.com>
2024-06-11 23:39:16 -0500

    divider

--- CharClass.cs
+++ CharClass.cs
@@ -657,6 +657,10 @@ This example produces the following results:
         char cHigh = '\uD800';
         char cLow = '\uDC00';
         string s1 = new String(new char[] { 'a', '\uD800', '\uDC00', 'z' });
+        string divider = String.Concat(Environment.NewLine, new String('-', 70),
+
+
+
     }
 
 
