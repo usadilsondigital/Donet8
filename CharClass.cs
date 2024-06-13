ffc241c71094adca7f95f04c5a4bad55c0210d67
Adilson <usadilsondigital@gmail.com>
2024-06-13 17:16:47 -0500

    console c3

--- CharClass.cs
+++ CharClass.cs
@@ -685,7 +685,7 @@ This example produces the following results:
         Console.WriteLine("Is each of the following pairs of characters a surrogate pair?");
         Console.WriteLine("C1) cHigh and cLow?  - {0}", Char.IsSurrogatePair(cHigh, cLow));
         Console.WriteLine("C2) s1[0] and s1[1]? - {0}", Char.IsSurrogatePair(s1, 0));
-
+        Console.WriteLine("C3) s1[1] and s1[2]? - {0}", Char.IsSurrogatePair(s1, 1));
 
 
 
